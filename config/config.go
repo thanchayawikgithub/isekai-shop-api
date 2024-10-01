cf33844f63e4549561bf16c58bacebd79e14bb89
thanchayawik <thanchayawik@gmail.com>
2024-10-01 14:56:02 +0700

    update config

---
+++ config/config.go
@@ -0,0 +1,83 @@
+package config
+
+import (
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/spf13/viper"
+)
+
+type (
+	Database struct {
+		Host     string `mapstructure:"host" validate:"required"`
+		Port     int    `mapstructure:"port" validate:"required"`
+		User     string `mapstructure:"user" validate:"required"`
+		Password string `mapstructure:"password" validate:"required"`
+		DBName   string `mapstructure:"dbname" validate:"required"`
+		SSLMode  string `mapstructure:"sslmode" validate:"required"`
+		Schema   string `mapstructure:"schema" validate:"required"`
+	}
+
+	Server struct {
+		Port         int           `mapstructure:"port" validate:"required"`
+		AllowOrigins []string      `mapstructure:"allowOrigins" validate:"required"`
+		Timeout      time.Duration `mapstructure:"timeout" validate:"required"`
+		BodyLimit    string        `mapstructure:"bodyLimit" validate:"required"`
+	}
+
+	OAuth2 struct {
+		PlayerRedirectUrl string    `mapstructure:"playerRedirectUrl" validate:"required"`
+		AdminRedirectUrl  string    `mapstructure:"adminRedirectUrl" validate:"required"`
+		ClientId          string    `mapstructure:"clientId" validate:"required"`
+		ClientSecret      string    `mapstructure:"clientSecret" validate:"required"`
+		Endpoints         *endpoint `mapstructure:"endpoints" validate:"required" `
+		Scopes            []string  `mapstructure:"scopes" validate:"required"`
+		UserInfoUrl       string    `mapstructure:"userInfoUrl" validate:"required"`
+		RevokeUrl         string    `mapstructure:"revokeUrl" validate:"required"`
+	}
+
+	endpoint struct {
+		AuthUrl       string `mapstructure:"authUrl" validate:"required"`
+		TokenUrl      string `mapstructure:"tokenUrl" validate:"required"`
+		DeviceAuthUrl string `mapstructure:"deviceAuthUrl" validate:"required"`
+	}
+
+	Config struct {
+		Database *Database `mapstructure:"database" validate:"required"`
+		Server   *Server   `mapstructure:"server" validate:"required"`
+		OAuth2   *OAuth2   `mapstructure:"oauth2" validate:"required"`
+	}
+)
+
+var (
+	once           sync.Once
+	configInstance *Config
+)
+
+func LoadCofig() *Config {
+	once.Do(func() {
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+		viper.AddConfigPath("./")
+		viper.AutomaticEnv()
+		viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+		if err := viper.ReadInConfig(); err != nil {
+			panic(err)
+		}
+
+		if err := viper.Unmarshal(&configInstance); err != nil {
+			panic(err)
+		}
+
+		validate := validator.New()
+
+		if err := validate.Struct(configInstance); err != nil {
+			panic(err)
+		}
+	})
+
+	return configInstance
+}
