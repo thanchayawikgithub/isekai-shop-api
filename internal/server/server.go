78ef28012e1ea15cf67afac35a9b57fdd79243fc
thanchayawik <thanchayawik@gmail.com>
2024-10-10 17:57:50 +0700

    update gracefully shutdown

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,9 +1,13 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -28,6 +32,7 @@ func NewEchoServer(conf *config.Config, db *gorm.DB) *echoServer {
 
 	app.Use(middleware.Logger())
 	app.Use(middleware.Recover())
+
 	app.Logger.SetLevel(log.DEBUG)
 
 	once.Do(func() {
@@ -43,7 +48,13 @@ func NewEchoServer(conf *config.Config, db *gorm.DB) *echoServer {
 
 func (s *echoServer) Start() {
 	s.app.GET("/v1/health", s.healthCheck)
+
+	quitCh := make(chan os.Signal, 1)
+	signal.Notify(quitCh, syscall.SIGINT, syscall.SIGTERM)
+	go s.gracefullyShutdown(quitCh)
+
 	s.httpListenting()
+
 }
 
 func (s *echoServer) httpListenting() {
@@ -56,4 +67,15 @@ func (s *echoServer) httpListenting() {
 
 func (s *echoServer) healthCheck(c echo.Context) error {
 	return c.String(http.StatusOK, "OK")
+}
+
+func (s *echoServer) gracefullyShutdown(quitCh chan os.Signal) {
+	ctx := context.Background()
+
+	<-quitCh
+	s.app.Logger.Info("Shutting down server")
+
+	if err := s.app.Shutdown(ctx); err != nil {
+		s.app.Logger.Fatalf("Error: %s", err.Error())
+	}
 }
