bea2d37662924586733969dbe922d40ed2eafcda
thanchayawik <thanchayawik@gmail.com>
2024-10-10 16:00:54 +0700

    use transaction to migrate database

--- databases/migration/migration.go
+++ databases/migration/migration.go
@@ -4,40 +4,48 @@ import (
 	"github.com/thanchayawikgithub/isekai-shop-api/config"
 	"github.com/thanchayawikgithub/isekai-shop-api/databases"
 	"github.com/thanchayawikgithub/isekai-shop-api/entities"
+	"gorm.io/gorm"
 )
 
 func main() {
 	conf := config.LoadCofig()
 	db := databases.NewPostgresDatabase(conf.Database)
 
-	playerMigration(db)
-	adminMigration(db)
-	itemMigration(db)
-	playerCoinMigration(db)
-	inventoryMigration(db)
-	purchaseHistoryMigration(db)
+	tx := db.Connect().Begin()
+
+	playerMigration(tx)
+	adminMigration(tx)
+	itemMigration(tx)
+	playerCoinMigration(tx)
+	inventoryMigration(tx)
+	purchaseHistoryMigration(tx)
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		panic(err)
+	}
 }
 
-func playerMigration(db databases.Database) {
-	db.Connect().Migrator().CreateTable(&entities.Player{})
+func playerMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.Player{})
 }
 
-func adminMigration(db databases.Database) {
-	db.Connect().Migrator().CreateTable(&entities.Admin{})
+func adminMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.Admin{})
 }
 
-func itemMigration(db databases.Database) {
-	db.Connect().Migrator().CreateTable(&entities.Item{})
+func itemMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.Item{})
 }
 
-func playerCoinMigration(db databases.Database) {
-	db.Connect().Migrator().CreateTable(&entities.PlayerCoin{})
+func playerCoinMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.PlayerCoin{})
 }
 
-func inventoryMigration(db databases.Database) {
-	db.Connect().Migrator().CreateTable(&entities.Inventory{})
+func inventoryMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.Inventory{})
 }
 
-func purchaseHistoryMigration(db databases.Database) {
-	db.Connect().Migrator().CreateTable(&entities.PurchaseHistory{})
+func purchaseHistoryMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.PurchaseHistory{})
 }
